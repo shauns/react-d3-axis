9b7de43e222dc1cf365ee0d38df0db016a57cd88
Shaun Stanworth <shaun.stanworth@googlemail.com>
2017-01-24 14:52:39 +0000

    Rename export

--- src/react-d3-axis.js
+++ src/react-d3-axis.js
@@ -1,5 +1,5 @@
 // @flow
-export {default as axisPropsFromScale} from './axisPropsFromTickScale';
+export {default as axisPropsFromTickScale} from './axisPropsFromTickScale';
 export {default as axisPropsFromBandedScale} from './axisPropsFromBandedScale';
 export {default as Axis, TOP, RIGHT, BOTTOM, LEFT} from './Axis';
 export type {TickScale, BandedScale, Scaler, Orients, AxisStyle} from './types';
